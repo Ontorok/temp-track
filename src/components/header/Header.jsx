78ece90f04d4b6467d7e597f37f87ef869aa4f9d
Nasir Ahmed <nasir.ahmed@bizzntek.com>
2024-11-09 16:11:01 +0600

    feat: add fav modal toggle feature

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import Favourite from "./Favourite";
 import FavouriteListModal from "./FavouriteListModal";
 import Logo from "./Logo";
 import Search from "./Search";
 
 const Header = () => {
+  const [isShowFavModal, setIsShowFavModal] = useState(false);
+
+  const handleFavModalToggle = () => {
+    setIsShowFavModal((prev) => !prev);
+  };
   return (
     <header className="fixed w-full top-0 left-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
       <nav className="container flex items-center justify-between py-6">
@@ -11,8 +17,8 @@ const Header = () => {
 
         <div className="flex items-center gap-4 relative">
           <Search />
-          <Favourite />
-          <FavouriteListModal />
+          <Favourite onFavModalToggle={handleFavModalToggle} />
+          {isShowFavModal && <FavouriteListModal />}
         </div>
       </nav>
     </header>
--- src/components/weather/AddToFavourite.jsx
+++ src/components/weather/AddToFavourite.jsx
@@ -1,27 +1,35 @@
 import { useFavouriteContext, useWeatherContext } from "../../hooks";
+
+const regularHeartIcon = "./assets/heart.svg";
+const redHeartIcon = "./assets/heart-red.svg";
+
 const AddToFavourite = () => {
   const { favourites, addToFavourites, removeFromFavourites } =
     useFavouriteContext();
   const { weather } = useWeatherContext();
+  const { latitude, longitude, location } = weather;
 
-  const handleAddToFavourite = () => {
-    const isInFavourites = favourites.find(
-      (f) => f.location === weather.location
-    );
-    console.log({ isInFavourites, favourites, weather });
+  const isInFavourites = favourites.find(
+    (f) => f.location === weather.location
+  );
+
+  const handleFavourite = () => {
     if (isInFavourites) {
       removeFromFavourites(weather.location);
     } else {
-      addToFavourites(weather.latitude, weather.longitude, weather.location);
+      addToFavourites(latitude, longitude, location);
     }
   };
 
   return (
-    <div className="md:col-span-2" onClick={handleAddToFavourite}>
+    <div className="md:col-span-2">
       <div className="flex items-center justify-end space-x-6">
-        <button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]">
+        <button
+          className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]"
+          onClick={handleFavourite}
+        >
           <span>Add to Favourite</span>
-          <img src="./assets/heart.svg" alt="" />
+          <img src={isInFavourites ? redHeartIcon : regularHeartIcon} alt="" />
         </button>
       </div>
     </div>
